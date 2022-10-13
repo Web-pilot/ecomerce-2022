34f4d821c2da34b28dee3b71a894a80aa77623de
ochagwu paul <ochagwupaul295@gmail.com>
2022-10-13 10:05:02 +0100

    still building...

--- jumia-replica/src/components/Account/Account.js
+++ jumia-replica/src/components/Account/Account.js
@@ -1,25 +1,24 @@
 import "./Account.css";
 
-import AccountGrids from "../Grids/AccountGrids/AccountGrids";
+import AccountGrids from "../AccountGrids/AccountGrids";
 import Accountsidebar from "../AccountSideBar/AccountSidebar";
-import AddressBook from "../Grids/AddressBook/AddressBook";
-import CreditStore from "../Grids/CreditStore/CreditStore";
-
+import AddressBook from "../AddressBook/AddressBook";
+import CreditStore from "../CreditStore/CreditStore";
 
 const Account = () => {
   return (
     <section className="container account_container">
-        <Accountsidebar />
-        <div className="main_content">
-          <h3 className="main_content_title">Account Overview</h3>
-          <div className="grids">
-            <AccountGrids />
-           <AddressBook />
-           <CreditStore />
-          </div>
+      <Accountsidebar />
+      <div className="main_content">
+        <h3 className="main_content_title">Account Overview</h3>
+        <div className="grids">
+          <AccountGrids />
+          <AddressBook />
+          <CreditStore />
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
--- jumia-replica/src/components/Hero/MainHero.js
+++ jumia-replica/src/components/Hero/MainHero.js
@@ -1,17 +1,16 @@
 import Carousel from "../Carousel/Carousel";
 import Categories from "../Categories/Categories";
 import "./MainHero.css";
-import Right from "../../Right.js/Right";
+import Right from "../Right/Right";
 
-const MainHero = ({categoryOpen}) => {
+const MainHero = ({ categoryOpen }) => {
   return (
     <section className="container main_hero_container">
-        <Categories />
-        <Carousel />
-        <Right />
-       
+      <Categories />
+      <Carousel />
+      <Right />
     </section>
-  )
-}
+  );
+};
 
-export default MainHero
+export default MainHero;
